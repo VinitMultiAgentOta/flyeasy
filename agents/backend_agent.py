986d661b4f9f54b7da8623274386bc2ded383c08
VinitMultiAgentOta <planyourticket@gmail.com>
2026-03-06 18:10:20 +0530

    fix: correct model name in backend_agent to groq/llama-3.3-70b-versatile

--- agents/backend_agent.py
+++ agents/backend_agent.py
@@ -3,7 +3,7 @@ from crewai import Agent, Task, LLM
 from architect_agent import architect_task
 
 llm = LLM(
-    model="ggroq/llama3-8b-8192",
+        model="groq/llama-3.3-70b-versatile",
     temperature=0.1,
     api_key=os.environ["GROQ_API_KEY"]
 )
